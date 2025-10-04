a4c07717618961193caa51d19fe5b80c36345fde
Mark-Malysa <mbmalysa@gmail.com>
2025-10-04 14:48:16 -0400

    Update main.py

--- jobjitsu_backend/main.py
+++ jobjitsu_backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) 
 
 # Include routers
 app.include_router(auth.router, prefix="/api", tags=["Authentication"])
