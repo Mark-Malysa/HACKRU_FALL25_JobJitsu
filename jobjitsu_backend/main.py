b52afc1708b111d4237b7527278f32d38e3797a7
Mark-Malysa <mbmalysa@gmail.com>
2025-10-05 11:20:12 -0400

    Update main.py

--- jobjitsu_backend/main.py
+++ jobjitsu_backend/main.py
@@ -18,7 +18,9 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Frontend URLs
+    allow_origins=["http://localhost:3000", 
+    "http://127.0.0.1:3000",
+    "https://hackru-fall-25-job-jitsu.vercel.app"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
